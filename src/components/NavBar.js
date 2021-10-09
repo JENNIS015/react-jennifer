0ffff3eb8b16e875afd16270125432ee9d3a40a6
jennis015 <jennis015@hotmail.com>
2021-10-08 21:36:51 -0300

    menu

---
+++ src/components/NavBar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import "../css/style.css"
+
+
+const NavBar = () => {
+    return (
+        <nav className="Navbar">
+            <h2>La Tienda</h2>
+            <ul>
+            <li>
+                <a href="/acerca">Acerca</a>
+            </li>
+            <li>
+                <a href="/projecto">Productos</a>
+            </li>
+            <li>
+                <a href="/contacto">Contacto</a>
+            </li>
+            </ul>
+       </nav>
+    )
+}
+export default NavBar;
