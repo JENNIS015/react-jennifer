2e5b78680d50046930a71ecc5b967ca1869c8ea4
jennis015 <jennis015@hotmail.com>
2021-12-06 19:15:39 -0300

    ok

--- src/App.js
+++ src/App.js
@@ -12,23 +12,15 @@ import NavBar from "./components/navbar/NavBar";
 import Footer from "./components/footer/Footer";
 
 function App() {
-
   return (
     <div>
       <CartContextProvider>
         <BrowserRouter>
           <NavBar />
-         
-<<<<<<< HEAD
-          
-=======
-
->>>>>>> 0a83d6846ada5f2e9ee7121a159ff9d570311fef
           <Switch>
-          <Homepage />
             <Route exact path='/'>
-            <Homepage />
-            <ItemListContainer />
+              <Homepage />
+              <ItemListContainer />
             </Route>
             <Route exact path='/productos' component={ItemListContainer} />
             <Route exact path='/categoria/:id' component={ItemListContainer} />
@@ -37,7 +29,6 @@ function App() {
             <Route exact path='/sucess' component={Sucess} />
           </Switch>
           <Footer />
-
         </BrowserRouter>
       </CartContextProvider>
     </div>
