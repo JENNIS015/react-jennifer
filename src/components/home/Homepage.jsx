550f774ef66c7c9db7c26b47a18d64cec125b9a4
jennis015 <jennis015@hotmail.com>
2021-12-07 15:31:14 -0300

    elimino carrousel

--- src/components/home/Homepage.jsx
+++ src/components/home/Homepage.jsx
@@ -1,35 +1,14 @@
-import React from "react";
 import { NavLink } from "react-router-dom";
-import { useEffect } from "react";
-
-import M from 'materialize-css/dist/js/materialize.min.js';
 
 const Homepage = () => {
-  useEffect(() => {
-    let carouselElem = document.querySelectorAll('.carousel.carousel-slider');
-        
-        
-  M.Carousel.init(carouselElem,{
-      indicators : true,
-      fullWidth : true,
-      duration : 150
-  });
 
-    setTimeout(()=>{
-     M.Carousel.getInstance(carouselElem[0]).next();
-  },4000);
-
-}, [])
-   
   return (
     <div>
       <div className='carousel carousel-slider'>
         <NavLink className='carousel-item' to='/productos'>
-          <img src='/assets/image/portada.jpg' alt="MarketBA" />
+          <img src='/assets/image/portada.jpg' alt='MarketBA' />
         </NavLink>
-        <NavLink className='carousel-item' to='/productos'>
-          <img src='/assets/image/portada2.jpg' alt="MarketBA" />
-        </NavLink>
+      
       </div>
 
       <div className='container'>
