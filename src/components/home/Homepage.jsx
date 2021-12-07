ceba1064a9b4a01a6ec90eaf6ad45ccd00449fa0
jennis015 <jennis015@hotmail.com>
2021-12-07 12:56:41 -0300

    ok

--- src/components/home/Homepage.jsx
+++ src/components/home/Homepage.jsx
@@ -1,23 +1,6 @@
 import { NavLink } from "react-router-dom";
-import { useEffect } from "react";
-import M from 'materialize-css/dist/js/materialize.min.js';
 
 const Homepage = () => {
-//   useEffect(() => {
-//     let carouselElem = document.querySelectorAll('.carousel.carousel-slider');
-        
-//   M.Carousel.init(carouselElem,{
-//       indicators : true,
-//       fullWidth : true,
-//       duration : 150
-//   });
-
-//     setTimeout(()=>{
-//      M.Carousel.getInstance(carouselElem[0]).next();
-//   },4000);
-
-// }, [])
-   
   return (
     <div>
       <div className='carousel carousel-slider'>
