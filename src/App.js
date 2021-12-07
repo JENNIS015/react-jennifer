bc23ca26abd90e2a39def70e305613784713b269
jennis015 <jennis015@hotmail.com>
2021-12-07 11:07:50 -0300

    ok

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   return (
     <div>
       <CartContextProvider>
-        <BrowserRouter basename="/">
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
           <NavBar />
 
           <Switch>
