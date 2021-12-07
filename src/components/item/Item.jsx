893d347c2f81c6a8044ee2279df88962aad60b9b
jennis015 <jennis015@hotmail.com>
2021-12-07 18:21:43 -0300

    img url

--- src/components/item/Item.jsx
+++ src/components/item/Item.jsx
@@ -4,14 +4,14 @@ import { useCartContext } from "../../context/CartContext";
 
 const Item = ({ prod }) => {
   const { formatNumber } = useCartContext();
-   
+   const imgUrl= process.env.PUBLIC_URL + prod.urlImagen
   return (
     <Link to={`/item/${prod.id}`}>
       <div className='col s12 m6 l4' key={prod.id}>
         <div className='card hoverable'>
           <div className='card-image waves-effect waves-block waves-light'>
             
-            <img src={prod.urlImagen} alt={prod.nombre} />
+            <img src={imgUrl} alt={prod.nombre} />
           </div>
 
           <div className='card-content'>
