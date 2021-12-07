da061a26bc225c9bbbdca172225376889899b985
jennis015 <jennis015@hotmail.com>
2021-12-07 10:16:57 -0300

    url

--- src/components/container/itemListContainer/ItemListContainer.jsx
+++ src/components/container/itemListContainer/ItemListContainer.jsx
@@ -7,32 +7,35 @@ import Loading from "../../loading/Loading";
 import { useLocation } from "react-router-dom";
 
 export const ItemListContainer = () => {
-  {console.log("paso2")}
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
   const { id } = useParams();
   const location = useLocation();
- 
+
   useEffect(() => {
-    let isMounted = true; 
-    var query = getFirestore().collection("items")
+    let isMounted = true;
+    var query = getFirestore().collection("items");
     if (id) {
       query = query.where("categoria", "==", id);
-   
     } else if (location.pathname === "/") {
       query = query.where("ofertaEspecial", "==", true);
     }
-    {console.log("paso3")}
+
     query
       .get()
-      .then((resp) =>{if(isMounted) 
-        setProducts(resp.docs.map((prod) => ({ id: prod.id, ...prod.data() })));   
+      .then((resp) => {
+        if (isMounted)
+          setProducts(
+            resp.docs.map((prod) => ({ id: prod.id, ...prod.data() }))
+          );
       })
       .catch((err) => console.log(err))
       .finally(() => setLoading(false));
-    
-      return () => { isMounted = false };  
-  }, [id,location]);
+
+    return () => {
+      isMounted = false;
+    };
+  }, [id, location]);
 
   return loading ? (
     <Loading />
--- src/components/home/Homepage.jsx
+++ src/components/home/Homepage.jsx
@@ -1,33 +1,33 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-// import { useEffect } from "react/cjs/react.development";
-// import M from 'materialize-css/dist/js/materialize.min.js';
+import { useEffect } from "react/cjs/react.development";
+import M from 'materialize-css/dist/js/materialize.min.js';
 
 const Homepage = () => {
-//   useEffect(() => {
-//     let carouselElem = document.querySelectorAll('.carousel.carousel-slider');
+  useEffect(() => {
+    let carouselElem = document.querySelectorAll('.carousel.carousel-slider');
         
-//   M.Carousel.init(carouselElem,{
-//       indicators : true,
-//       fullWidth : true,
-//       duration : 150
-//   });
+  M.Carousel.init(carouselElem,{
+      indicators : true,
+      fullWidth : true,
+      duration : 150
+  });
 
-//     setTimeout(()=>{
-//      M.Carousel.getInstance(carouselElem[0]).next();
-//   },4000);
+    setTimeout(()=>{
+     M.Carousel.getInstance(carouselElem[0]).next();
+  },4000);
 
-// }, [])
+}, [])
    
   return (
     <div>
       <div className='carousel carousel-slider'>
         <NavLink className='carousel-item' to='/productos'>
-        {console.log("paso1")}
-          <img src='/assets/image/portada.jpg' alt="MarketBA" />
+ 
+          <img src='assets/image/portada.jpg' alt="MarketBA" />
         </NavLink>
         <NavLink className='carousel-item' to='/productos'>
-          <img src='/assets/image/portada2.jpg' alt="MarketBA" />
+          <img src='assets/image/portada2.jpg' alt="MarketBA" />
         </NavLink>
       </div>
 
